7ba3d25e96729ac6f44f975ad315ab1c340f44ce
hyz <498371067@qq.com>
2021-03-17 23:43:22 +0800

    test

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -4,6 +4,6 @@
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecode' | xargs rm -rf
 
 # 日常任务
-node /scripts/jd_bean_home.js
+node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
 echo "jd_bean_home.js"
 #0 */2 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
