eff971353af5360b77cd34368dc075434fd89964
hyz <498371067@qq.com>
2021-03-17 23:31:25 +0800

    log

--- my_crontab_list.sh
+++ my_crontab_list.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
+
 # 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecode' | xargs rm -rf
 
 # 日常任务
-sleep 0
 node /scripts/jd_bean_home.js
+echo "jd_bean_home.js"
 #0 */2 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
