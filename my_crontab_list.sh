60f8fbf457f067e8b0fb59380af05a14e9dc1181
hyz <498371067@qq.com>
2021-03-17 21:12:53 +0800

    add: init

---
+++ my_crontab_list.sh
@@ -0,0 +1,5 @@
+# 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
+50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecode' | xargs rm -rf
+
+# 日常任务
+0 */2 * * * node /scripts/jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
